6bd0610852fe91a09cd7601be4f668c36706c05c
Yevhenii2022 <3starosta3@gmail.com>
2025-09-16 15:57:30 +0300

    init

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: Home
+*/
+
+get_header();
+?>
+
+<main>
+  <?php get_template_part('template-parts/hero-slider'); ?>
+
+  <?php get_template_part('template-parts/firm-section'); ?>
+
+</main>
+
+<?php get_footer(); ?>
